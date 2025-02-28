ee719b07a03c2d087725f2facc7363cfac43373c
hhhh22081 <matveev0711@gmail.com>
2025-02-28 21:52:08 +0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+function redirectToPurchasePage() {
+    window.location.href ="HTML/smartphone.html";
+}
+
+function redirectToPurchasePage2() {
+    window.location.href ="HTML/Apple MacBook.html";
+}
+
+function redirectToPurchasePage3() {
+    window.location.href ="HTML/Apple iPad Pro.html";
+}
+
+function redirectToPurchasePage4() {
+    window.location.href ="HTML/Apple Watch.html";
+}
+
+
